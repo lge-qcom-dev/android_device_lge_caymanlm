6e92e9f4f09e8e7b4b1c24759216bd362f2da3e5
pnguyen879 <howardson2001@gmail.com>
2026-02-01 18:56:08 -0600

    caymanlm: add a shim for libets_teeclient_v2
    The LGE resolution control never had any effect on fp and after the
    QPR2 update broke fp functionality, so the entire LGE stack is
    dropped, including libfpsph.
    
    However, libets_teeclient_v2 still depends on libfpsph, so a shim is
    introduced to stub out the LGE-specific functions in
    libets_teeclient_v2.
    
    Change-Id: Ibf98b9e68bce2b088402cc800cdb84988359007f

--- extract-files.py
+++ extract-files.py
@@ -19,12 +19,15 @@ from extract_utils.fixups_lib import (
 import extract_utils.tools
 
 namespace_imports = [
+    'hardware/lge',
     'vendor/lge/sm7250-common',
     'vendor/qcom/opensource/display',
 ]
 
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/lib64/libets_teeclient_v2.so': blob_fixup()
+        .replace_needed('libfpsph.so', 'libets_teeclient_v2_shim.so'),
     'vendor/lib64/liblgdnnsnpe.so': blob_fixup()
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
     'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so': blob_fixup()
